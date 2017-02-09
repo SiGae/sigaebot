5e2289bd5a273dbe06e4f6d409348e25aadee5ab
DESKTOP-MD0JUH3\alstn <alstn1601@naver.com>
2017-02-10 00:21:53 +0900

    최초 생성

---
+++ sigaebot.py
@@ -0,0 +1,46 @@
+import telebot
+from datetime import datetime
+import random
+import time
+import threading
+import sigaebot_private as bot_private
+
+thread_active = False
+
+
+def tr():
+    global thread_active
+    thread_active = True
+    time.sleep(30)
+    thread_active = False
+
+
+def remove_none(text):
+    if text is None:
+        return ""
+    else:
+        return text
+
+answer = ['Yes', 'No']
+bot = telebot.TeleBot(bot_private.id['botid'])
+
+
+@bot.message_handler(commands=['pink'])
+def check_activate(message):
+    bot.reply_to(message, "bot actvation is stable")
+
+
+def todaytime() -> str:
+    dt = datetime.today()
+    text = '{0}시 {1}분 {2}초 '.format(dt.hour, dt.minute, dt.second)
+    return text
+
+
+@bot.message_handler(func=lambda alwaysTrue:True)
+def always(message):
+    try:
+        print(message, end='\n\n')
+    except Exception as e:
+        print(e)
+
+bot.polling()
