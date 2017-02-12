1429cccf151006ea6c8d4db47aaafa2f79c83516
unknown <>
2017-02-12 23:15:29 +0900

    특정 커맨드에 반응하는 기능추가, 특정 단어가 채팅창에 올라오면 특정유저에게 개인메세지 선송할수 있음

--- sigaebot.py
+++ sigaebot.py
@@ -1,18 +1,10 @@
 import telebot
-from datetime import datetime
-import random
 import time
 import threading
 import sigaebot_private as bot_private
 
 thread_active = False
 
-
-def tr():
-    global thread_active
-    thread_active = True
-    time.sleep(30)
-    thread_active = False
 
 
 def remove_none(text):
@@ -21,30 +13,46 @@ def remove_none(text):
     else:
         return text
 
-answer = ['Yes', 'No']
+
 bot = telebot.TeleBot(bot_private.id['botid'])
 
 
 @bot.message_handler(commands=['pink'])
 def check_activate(message):
-    bot.reply_to(message, "bot activation is stable")
+    bot.reply_to(message, "bot activation is stable ㄴ")
 
+def tr():
+    global thread_active
+    thread_active = True
+    time.sleep(30)
+    thread_active = False
 
-def todaytime() -> str:
-    dt = datetime.today()
-    text = '{0}시 {1}분 {2}초 '.format(dt.hour, dt.minute, dt.second)
-    return text
 
 
 @bot.message_handler(func=lambda alwaysTrue: True)
 def alwaysTure(message):
     try:
         print(message, end='\n\n')
+        for key1, value1 in bot_private.callHuman.items():
+            for key0, value0 in bot_private.id.items():
+                if key1 in message.text:
+                    if value1 == key0:
+                        bot.send_message(str(value0),key1 + "님 {}가 너 불러 \n 메세지 내용 : {}".format(
+                            str(message.from_user.first_name) + remove_none(message.from_user.last_name), message.text))
+                        print("call")
         for key, value in bot_private.command.items():
             if key in message.text:
-                bot.reply_to(message, value)
+                bot.send_message(str(message.chat.id), value())
+                break
+                return
+        if "ㅋㅋㅋㅋㅋㅋㅋㅋ" in message.text:
+            if not thread_active:
+                t = threading.Thread(target=tr)
+                bot.send_message(message.chat.id, 'ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ')
+                t.start()
 
     except Exception as e:
         print(e)
+
 
 bot.polling()
