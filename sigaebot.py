c7a6da2e9c7adab627098713d9bf087d11e7742f
DESKTOP-I51FQOH\alstn <alstn1601@naver.com>
2017-02-14 00:42:27 +0900

    정규식 지원

--- sigaebot.py
+++ sigaebot.py
@@ -2,10 +2,10 @@
 import telebot
 import time
 import threading
+import re
 import sigaebot_private as bot_private
 
 thread_active = False
-
 
 
 def remove_none(text):
@@ -18,9 +18,10 @@ def remove_none(text):
 bot = telebot.TeleBot(bot_private.id['botid'])
 
 
-@bot.message_handler(commands=['pink'])
+@bot.message_handler(commands=['ping'])
 def check_activate(message):
     bot.reply_to(message, "bot activation is stable ㄴ")
+
 
 def tr():
     global thread_active
@@ -29,18 +30,19 @@ def tr():
     thread_active = False
 
 
-
 @bot.message_handler(func=lambda alwaysTrue: True)
 def alwaysTure(message):
     try:
         print(message, end='\n\n')
-        for key1, value1 in bot_private.callHuman.items():
-            for key0, value0 in bot_private.id.items():
-                if key1 in message.text:
-                    if value1 == key0:
-                        bot.send_message(str(value0),key1 + "님 {}가 너 불러 \n 메세지 내용 : {}".format(
-                            str(message.from_user.first_name) + remove_none(message.from_user.last_name), message.text))
-                        print("call")
+        for key0, value0 in bot_private.id.items():
+            for key2, value2 in bot_private.regex.items():
+                target_reg = re.compile(key2)
+                if (target_reg.match(message.text) and value2 == key0) and message.from_user.id != value0:
+                    bot.send_message(str(value0), "님 {}가 너 불러 \n 메세지 내용 : {}".format(
+                                    str(message.from_user.first_name) + remove_none(message.from_user.last_name), message.text))
+                    print("call")
+                    return
+
         for key, value in bot_private.command.items():
             if key in message.text:
                 bot.reply_to(message, value())
@@ -53,6 +55,5 @@ def alwaysTure(message):
 
     except Exception as e:
         print(e)
-
 
 bot.polling()
