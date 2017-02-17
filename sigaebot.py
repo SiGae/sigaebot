18db44797d9d366c9f81ce52bc374a45f0dd8981
DESKTOP-I51FQOH\alstn <alstn1601@naver.com>
2017-02-17 22:12:00 +0900

    채팅방안에 없는 사람에 해당되는 키워드가 작동되는 버그를 수정했습니다

--- sigaebot.py
+++ sigaebot.py
@@ -16,11 +16,12 @@ def remove_none(text):
 
 
 bot = telebot.TeleBot(bot_private.id['botid'])
-
+memberlist = ["creator", "administrator", "member"]
 
 @bot.message_handler(commands=['ping'])
 def check_activate(message):
     bot.reply_to(message, "bot activation is stable ㄴ")
+    bot.reply_to(message, bot.get_chat_member(message.chat.id, message.from_user.id))
 
 
 def tr():
@@ -37,11 +38,19 @@ def alwaysTure(message):
         for key0, value0 in bot_private.id.items():
             for key2, value2 in bot_private.regex.items():
                 target_reg = re.compile(key2)
-                if (target_reg.match(message.text) and value2 == key0) and message.from_user.id != value0:
-                    bot.send_message(str(value0), "님 {}가 너 불러 \n 메세지 내용 : {}".format(
+                if (target_reg.search(message.text) and value2 == key0)\
+                        and "r" in bot.get_chat_member(message.chat.id, value0).status\
+                        and message.from_user.id != value0:
+                    bot.send_message(value0, "님 {}가 너 불러 \n 메세지 내용 : {}".format(
                                     str(message.from_user.first_name) + remove_none(message.from_user.last_name), message.text))
                     print("call")
                     return
+
+#test
+#        if "월요일" in message.text:
+ #           bot.send_message(message.chat.id, "뭐? 월요일? 뒤지고 싶냐")
+#            bot.kick_chat_member(message.chat.id,message.from_user.id )
+
 
         for key, value in bot_private.command.items():
             if key in message.text:
@@ -50,7 +59,7 @@ def alwaysTure(message):
             if "ㅋㅋㅋㅋㅋㅋㅋㅋ" in message.text:
                 if not thread_active:
                     t = threading.Thread(target=tr)
-                    bot.send_message(message.chat.id, 'ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ')
+                    bot.send_message(message.chat.id, 'ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ')
                     t.start()
 
     except Exception as e:
