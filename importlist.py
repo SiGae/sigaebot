e43a74f9b505bc25b96345af33d15bd71ada6a6f
SIGAE\alstn <alstn1601@naver.com>
2017-07-09 00:40:25 +0900

    #db를 사용한 기능 추가

--- importlist.py
+++ importlist.py
@@ -3,5 +3,4 @@ import time
 import re
 from datetime import datetime
 import random
-from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import pymysql
--- sigaebot.py
+++ sigaebot.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
-import importlist as code
 import sigaebot_private as bot_private
+import importlist as code
+import dbconnect as db
 
 answer = ['ㅇㅇ', 'ㄴㄴ']
 
@@ -15,7 +16,8 @@ def remove_none(text):
         return text
 
 
-bot = code.telebot.TeleBot(bot_private.id['botid'])
+
+bot = bot_private.code.telebot.TeleBot(bot_private.id['botid'])
 
 
 @bot.message_handler(commands=['ping'])
@@ -43,11 +45,6 @@ def quickcommand(message):
 def namuwiki(message):
     url = 'https://namu.wiki/w/{0}'.format(message.text.replace("/namu ", "").replace(" ","%20").strip())
     bot.send_message(message.chat.id, url)
-    #driver = code.webdriver.Firefox(executable_path="/home/pi/geckodriver")
-    #driver.get(url)
-    #bot.send_photo(message.chat.id, driver.get_screenshot_as_png())
-    #driver.close()
-
 
 @bot.message_handler(func=lambda Alwaystrue: True)
 def always(message):
@@ -63,12 +60,33 @@ def always(message):
                                     str(message.from_user.first_name)
                                     + remove_none(message.from_user.last_name), message.text))
                     print("call")
-
+        #for key, value in bot_private.banword.items():
+            #if key in message.text:
+                #bot.send_message(message.chat.id, message.text.replace(key,value))
+                #bot.edit_message_text(message.text.replace(key,value),message.chat.id, message.message_id)
+                #bot.delete_message(message.chat.id, message.message_id)
         for key, value in bot_private.command.items():
             if key in message.text:
                 bot.reply_to(message, value())
                 return
+        for key, value in bot_private.regex_username.items():
+            target_reg = code.re.compile(key)
+            if "죽인다" in message.text and target_reg.search(message.text):
+                killerName = message.from_user.namename
+                diedName = value
+                curs = db.conn.cursor()
+                getKillCount = '' #kill_count 값을 가져오는 SQL 문 (SELECT 문 사용)
+                killCount = curs.execute(getKillCount) + 1
+                getDeathCount = '' #death_count 값을 가져오는 SQL 문 (SELECT 문 사용
+                deathCount = curs.execute(getDeathCount) + 1
+                #killCount변수의 값을 killName의 kill_count에 추가하는 구문 (UPDATE 문 사용)
+                #deathCount변수의 값을 diedName의 death_count에 추가하는 구문 (UPDATE 문 사용)
 
+                db.conn.close()
+
+
+            #bot_private.kill_count+=1
+            #bot.send_message(message.chat.id, "{0}의 킬카운트가 올라갔습니다\n현재 킬카운트 : {1}".format(message.from_user.first_name,bot_private.kill_count))
         if Anti_monday:
             for key, value in bot_private.ban.items():
                 if key in message.text:
